2a0f524df574a2b77b4efbcd3ca3229d445d8b22
koyu <me@koyu.space>
2023-10-16 05:10:54 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 git pull
+sudo apt update
+sudo apt dist-upgrade -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 npm i
 python3 backend.py &
